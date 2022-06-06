e180d13ce0ed5b07e0d3abc1f07a0db5ca2b983d
kiran <kiran@DESKTOP-KSV9KVJ>
2022-06-06 23:31:42 +0530

    commit

---
+++ src/abstract_concrete/abstract_.java
@@ -0,0 +1,18 @@
+package abstract_concrete;
+
+public abstract class abstract_ {
+	public static void main(String[] args) {
+		
+	
+		
+		
+	}
+	public abstract void  m1();
+	public abstract void m2();
+	public void m3() {
+		System.out.println("m3 method running");
+	}
+	
+	
+
+}
